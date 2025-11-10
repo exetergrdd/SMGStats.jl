2e77db2c8bacfe9d6454265825c49710969f2607
Nick Owens <owensnick@gmail.com>
2025-11-10 12:05:12 +0000

    update modhist inline with redesign

--- src/stats/modhist.jl
+++ src/stats/modhist.jl
@@ -1,27 +1,28 @@
 
 ############################# modifcation hist #############################
+
+
 struct ModHist <: RecordStat
-    data::Dict{Modification, Vector{Int}}
+    data::Matrix{Int}
 end
-ModHist() = ModHist(Dict{Modification, Vector{Int}}())
+ModHist() = ModHist(zeros(256, length(instances(Modification))))
+
+
+instantiate(::Type{ModHist}, reader, mods) = ModHist(zeros(256, length(instances(Modification))))
+recordupdates(::Type{ModHist}) = nothing
+modupdates(::Type{ModHist}) = ModUpdates
+postmodupdates(::Type{ModHist}) = nothing
+
 
 statname(::Type{ModHist}) = "Modification histogram" 
 
-function update!(stat::ModHist, reader::HTSFileReader, record::BamRecord, recorddata)
-    for mi in ModIterator(record, recorddata)
-        haskey(stat.data, mi.mod) || (stat.data[mi.mod] = zeros(Int, 256))
-        stat.data[mi.mod][mi.prob + 1] += 1
-    end
-end
+@inline updatemod!(stat::ModHist, mod::ModificationInfo, record::BamRecord, recorddata) = stat.data[mod.prob + 1, Int(mod.mod) + 1] += 1
 
 function writestats(stat::ModHist, path::String, file="modhist.tsv.gz")
     filepath = joinpath(path, file)
-    df = DataFrame(Dict(string(m) => v for (m, v) in stat.data))
-    modlabels = names(df)
-    df.ml = 0:255
-    df.prob = df.ml./255
-    df = df[!, ["ml" ; "prob" ; modlabels]]
-    # df = mapreduce((mod, vec) -> DataFrame(mod=mod, ml=ml, prob=prob, count=count), stat.data)
+    ind = sum(stat.data, dims=1) .> 0
+    df = [DataFrame(ml=0:255, prob=(0:255)/255) DataFrame(stat.data[:, ind], string.(instances(Modification)[ind]))]
+    
     CSV.write(filepath, df, delim='\t', compress=endswith(file, ".gz"))
 end
 function plotstat(::Type{ModHist}, df::DataFrame, scale=1e+6, thresh=0.9)
