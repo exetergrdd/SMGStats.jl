9c8e4a28b85a897c2fc97ee475ae3ee2e1a6e72a
Nick Owens <owensnick@gmail.com>
2025-11-10 12:04:36 +0000

    update exports

--- src/SMGStats.jl
+++ src/SMGStats.jl
@@ -10,10 +10,15 @@ using DataFrames, CSV
 
 using CairoMakie, AlgebraOfGraphics, ColorSchemes
 
-export firestats, unicodeplot, displayplots, writeallstats, readstats, plotstat, htmlreport, statname, samplesummary
+export firestats, unicodeplot, displayplots, writeallstats, readstats, plotstat, htmlreport, statname, samplesummary,
+        ChromStat, ModCrossCor, ModHist, ModMetaHist, ModRate, NucMSPLenHist, NucMSPRate, ReadLengthCounter,
+        @smgstats
+        
+
 
 # include("unicode_tools.jl")
 include("utils.jl")
+include("bitpacked.jl")
 include("stats/stats.jl")
 include("report.jl")
 end
