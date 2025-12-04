d73f0dc9dcd95bcc73410209e10a7aba273c2c9a
Nick Owens <owensnick@gmail.com>
2025-12-04 13:48:39 +0000

    added imports and exports

--- src/SMGStats.jl
+++ src/SMGStats.jl
@@ -7,18 +7,25 @@ using OnlineStats
 using ProgressMeter
 # using UnicodePlots
 using DataFrames, CSV
+using GenomicFeatures
+
+import YAML
 
 using CairoMakie, AlgebraOfGraphics, ColorSchemes
 
 export firestats, unicodeplot, displayplots, writeallstats, readstats, plotstat, htmlreport, statname, samplesummary,
-        ChromStat, ModCrossCor, ModHist, ModMetaHist, ModRate, NucMSPLenHist, NucMSPRate, ReadLengthCounter,
-        @smgstats
+        ChromStat, ModCrossCor, ModHist, ModMetaHist, ModRate, NucMSPLenHist, NucMSPRate, ReadLengthCounter, MetaPlot,
+        @smgstats, 
+        metaplot, loadregions,
+        loadmultichromintervals, loadstatconfig
         
 
 
 # include("unicode_tools.jl")
 include("utils.jl")
 include("bitpacked.jl")
+include("regions.jl")
 include("stats/stats.jl")
 include("report.jl")
+include("summarystats.jl")
 end
