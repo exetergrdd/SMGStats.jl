bc176e9b4a1cd6a79ec8d3af10aeaa2ded41a867
Nick Owens <owensnick@gmail.com>
2025-12-15 20:14:01 +0000

    updated stat compat and filtering stats

--- src/stats/statconfig.jl
+++ src/stats/statconfig.jl
@@ -14,10 +14,25 @@ function build_namedtuple(data)
     end
 end
 
+function warncompatible(f, a)
 
-function loadstatconfig(file)
+    if compatible(f, a)
+        return true
+    else
+        smglog("Warning stat: ", f, " removed due to incompatibility with file's ", a)
+        return false
+    end
+end
+function loadstatconfig(file, auxmap=nothing)
     yamlconfig = YAML.load_file(file)
-    stats = buildsmgstats(yamlconfig["stats"])
+
+    stattypes = [getfield(SMGStats, Symbol(s)) for s in yamlconfig["stats"]]
+    
+    if !isnothing(auxmap)
+        filter!(f -> warncompatible(f, typeof(auxmap)), stattypes)
+    end
+    stats = Tuple{stattypes...}
+    
     config = NamedTuple(Symbol(k) => build_namedtuple(c) for (k, c) in yamlconfig["configs"])
 
     stats, config
