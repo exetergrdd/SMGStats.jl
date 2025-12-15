eeaf174157c640abe69a229e5cb9fd91cb0be32b
Nick Owens <owensnick@gmail.com>
2025-12-15 20:08:38 +0000

    added imports exports

--- src/SMGStats.jl
+++ src/SMGStats.jl
@@ -15,13 +15,31 @@ using SMGReader
 using Statistics
 using StatsBase
 
+using ImageFiltering
+
 export firestats, unicodeplot, displayplots, writeallstats, readstats, plotstat, htmlreport, statname, samplesummary,
-        ChromStat, ModCrossCor, ModHist, ModMetaHist, ModRate, NucMSPLenHist, NucMSPRate, ReadLengthCounter, MetaPlot,
+        ChromStat, ModCrossCor, ModHist, ModMetaHist, ModRate, NucMSPLenHist, NucMSPRate, ReadLengthCounter, MetaPlot, NucMonoDiRatio,
         @smgstats, 
         metaplot, loadregions,
         loadmultichromintervals, loadstatconfig,
-        runstats
+        runstats,
+        combinereports
         
+
+
+
+"""
+    smglog(msg)
+
+Prints a timestamped, grep-friendly log line:
+
+    [2025-02-12T14:22:03] [SMGStats] <msg>
+"""
+function smglog(msg...)
+    msg = string(msg...)
+    ts = Dates.format(Dates.now(), dateformat"yyyy-mm-ddTHH:MM:SS")
+    println("[$ts] [SMGStats] $msg")
+end
 
 
 # include("unicode_tools.jl")
@@ -31,5 +49,7 @@ include("regions.jl")
 include("stats/stats.jl")
 include("report.jl")
 include("summarystats.jl")
+include("multireport.jl")
 include("main.jl")
+
 end
