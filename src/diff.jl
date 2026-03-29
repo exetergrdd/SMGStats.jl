b442484fd08b418adc5197fc6472d710617ac65f
Nick Owens <owensnick@gmail.com>
2026-03-29 12:55:06 +0100

    update differential scoring

--- src/diff.jl
+++ src/diff.jl
@@ -15,6 +15,34 @@
         return total_fire, total_read
     end
 
+
+
+function fisher(a, b, c, d)
+    try
+        f = FisherExactTest(a, b, c, d)
+        or = f.ω
+        logor_se = sqrt.(1.0./a .+ 1.0./b .+ 1.0./c .+ 1.0./d)
+        fisher_lpv = pvalue(f, tail=:left)
+        fisher_bpv = pvalue(f, tail=:both)
+        fisher_rpv = pvalue(f, tail=:right)
+        return (;or, logor_se, fisher_lpv, fisher_bpv, fisher_rpv)
+    catch
+        smglog("FisherExactTest failed for $a, $b, $c, $d")
+        return (; or=NaN, logor_se=NaN, fisher_lpv=NaN, fisher_bpv=NaN, fisher_rpv=NaN)
+    end
+
+end
+
+function binomialtest(x, n, p)
+   try
+        bt = BinomialTest(x, n, p)
+        return (;lpv=pvalue(bt, tail=:left), bpv=pvalue(bt, tail=:both), rpv=pvalue(bt, tail=:right))
+    catch
+        smglog("BinomialTest failed for $x, $n, $p")
+        return (; lpv=NaN, bpv=NaN, rpv=NaN)
+    end
+end
+
 """
     differentialaccessibility(bedfile, bamsA, bamsB, output; method=:fisher, chromlabel=:chrom, startlabel=:start, stoplabel=:stop)
 
@@ -28,9 +56,13 @@ Counts fires and reads for all files, pools them by group, and calculates p-valu
 - `output`: Output filename.
 - `method`: Statistical method (`:fisher`, `:binomial_pooled`, `:binomial_ref`, `:alll`).
 """
-function differentialaccessibility(bedfile::String, bamsA::Vector{String}, bamsB::Vector{String}, output::String;
-                      method=:fisher, chromlabel=:chrom, startlabel=:start, stoplabel=:stop)
+function differentialaccessibility(bedfile::String, bamsA, bamsB, output::String; method=:fisher, chromlabel=:chrom, startlabel=:start, stoplabel=:stop)
 
+    smglog("               Differential Accessibility Analysis")
+    smglog("Group A:      ", bamsA)
+    smglog("Group B:      ", bamsB)
+    smglog("Output:       ", output)
+    smglog("Method:       ", method)
     # Load peaks
     bdf = loadbed(bedfile)
     intervals = bedintervals(bdf; chromlabel=Symbol(chromlabel), startlabel=Symbol(startlabel), stoplabel=Symbol(stoplabel))
@@ -65,16 +97,17 @@ function differentialaccessibility(bedfile::String, bamsA::Vector{String}, bamsB
         # Group A   Fa      Ra - Fa
         # Group B   Fb      Rb - Fb
 
-        fts = [FisherExactTest(fireA[i], readA[i] - fireA[i], fireB[i], readB[i] - fireB[i]) for i in 1:length(intervals)]
+        # fts = [FisherExactTest(fireA[i], readA[i] - fireA[i], fireB[i], readB[i] - fireB[i]) for i in 1:length(intervals)]
+        fts = [fisher(fireA[i], readA[i] - fireA[i], fireB[i], readB[i] - fireB[i]) for i in 1:length(intervals)]
         bdf.a = fireA
         bdf.b = readA .- fireA
         bdf.c = fireB
         bdf.d = readB .- fireB
-        bdf.OR = [f.ω  for f in fts]
-        bdf.logor_se = sqrt.(1.0./bdf.a .+ 1.0./bdf.b .+ 1.0./bdf.c .+ 1.0./bdf.d)
-        bdf.fisher_lpv = pvalue(f, tail=:left)
-        bdf.fisher_bpv = pvalue(f, tail=:both)
-        bdf.fisher_rpv = pvalue(f, tail=:right)
+        bdf.OR = [f.or  for f in fts]
+        bdf.logor_se = [f.logor_se  for f in fts]
+        bdf.fisher_lpv = [f.fisher_lpv  for f in fts]
+        bdf.fisher_bpv = [f.fisher_bpv  for f in fts]
+        bdf.fisher_rpv = [f.fisher_rpv  for f in fts]
 
     end
         
@@ -83,19 +116,19 @@ function differentialaccessibility(bedfile::String, bamsA::Vector{String}, bamsB
         bdf.TotalReads = readA .+ readB
         bdf.TotalFires = fireA .+ fireB
         bdf.p_pool = bdf.TotalFires ./ bdf.TotalReads
-        bts = BinomialTest.(fireB, readB, bdf.p_pool)
-        bdf.binpool_lpv = pvalue(bts, tail=:left)
-        bdf.binpool_bpv = pvalue(bts, tail=:both)
-        bdf.binpool_rpv = pvalue(bts, tail=:right)
+        bts = [binomialtest(fireB[i], readB[i], bdf.p_pool[i]) for i in 1:length(intervals)]
+        bdf.binpool_lpv = [bt.lpv for bt in bts]
+        bdf.binpool_bpv = [bt.bpv for bt in bts]
+        bdf.binpool_rpv = [bt.rpv for bt in bts]
 
     end
     if method == :binomial_ref || method == :all
         # Reference rate derived from A: p = Fa / Ra
         bdf.p_ref = fireA ./ readA
-        bts = BinomialTest.(fireB, readB, bdf.p_ref)
-        bdf.binref_lpv = pvalue(bts, tail=:left)
-        bdf.binref_bpv = pvalue(bts, tail=:both)
-        bdf.binref_rpv = pvalue(bts, tail=:right)
+        bts = [binomialtest(fireB[i], readB[i], bdf.p_ref[i]) for i in 1:length(intervals)]
+        bdf.binref_lpv = [bt.lpv for bt in bts]
+        bdf.binref_bpv = [bt.bpv for bt in bts]
+        bdf.binref_rpv = [bt.rpv for bt in bts]
 
     end
 
