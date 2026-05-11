7039ba3f1d91c91758a2f81af2fcce9e204c1ef9
Nick Owens <owensnick@gmail.com>
2026-05-11 19:14:30 +0100

    added mod counting

--- src/countfires.jl
+++ src/countfires.jl
@@ -106,3 +106,143 @@ end
 
 
 
+
+
+
+function countmods(file, mod, intervals::GenomicIntervalCollection{GenomicInterval{T}}; total_reads=-1, fire_backing=5_000) where T
+
+    reader = open(HTSFileReader, file)
+    recorddata = StencillingData(AuxMapModFire())
+
+    ### fireinteresctions is a buffer of those intervals each read intersects with
+    fireintersections = VectorBuffer{GenomicInterval{T}}(fire_backing)
+
+
+    mod_counts   = zeros(Int(length(intervals)))
+    unmod_counts = zeros(Int(length(intervals)))
+
+    t = 0
+    for record in eachrecord(reader)
+        validflag(record) || continue
+        processread!(record, recorddata) || continue
+
+        ref = refname(reader, record)
+        readinterval = GenomicInterval(ref, SMGReader.leftposition(record) + 1, SMGReader.rightposition(recorddata)) ## convert to 1-based inclusive
+        fi = 0
+        setlength!(fireintersections, length(fireintersections.data))
+        for fiv in eachoverlap(intervals, readinterval)
+            fi += 1
+            fireintersections[fi] = fiv
+            k = GenomicFeatures.metadata(fiv)
+            # read_counts[k] += 1
+        end
+        setlength!(fireintersections, fi)
+
+        for mi in ModIterator(record, recorddata)
+            (mi.mod == mod) || continue
+            genomepos = genomecoords(mi.pos, record, recorddata)
+            @inbounds @simd for i = eachindex(fireintersections)
+                if (genomepos <= fireintersections[i].last) && (genomepos >= fireintersections[i].first)
+                    k = GenomicFeatures.metadata(fireintersections[i])
+                    if mi.prob >= 0.9 * 255
+                        mod_counts[k] += 1
+                    else
+                        unmod_counts[k] += 1
+                    end
+                end
+            end
+
+
+        end
+
+        t += 1
+        (t == total_reads) && break
+    end
+    close(reader)
+
+    mod_counts, unmod_counts
+
+end
+
+
+
+function countfiremods(file, intervals::GenomicIntervalCollection{GenomicInterval{T}}; total_reads=-1, fire_backing=5_000) where T
+
+    reader = open(HTSFileReader, file)
+    recorddata = StencillingData(AuxMapModFire())
+
+    ### fireinteresctions is a buffer of those intervals each read intersects with
+    fireintersections = VectorBuffer{GenomicInterval{T}}(fire_backing)
+    intersects = VectorBuffer{Bool}(fire_backing)
+
+    fields = [:fire, :mod_6mA, :mod_5mC, :mod_5hmC]
+    fg_counts = Dict(f => zeros(Int, length(intervals)) for f in fields)
+    total_counts = Dict(f => zeros(Int, length(intervals)) for f in fields)
+
+    
+
+    t = 0
+    for record in eachrecord(reader)
+        validflag(record) || continue
+        processread!(record, recorddata) || continue
+
+        ref = refname(reader, record)
+        readinterval = GenomicInterval(ref, SMGReader.leftposition(record) + 1, SMGReader.rightposition(recorddata)) ## convert to 1-based inclusive
+        fi = 0
+        setlength!(fireintersections, length(fireintersections.data))
+        for fiv in eachoverlap(intervals, readinterval)
+            fi += 1
+            fireintersections[fi] = fiv
+            k = GenomicFeatures.metadata(fiv)
+            total_counts[:fire][k] += 1
+        end
+        setlength!(fireintersections, fi)
+        setlength!(intersects, fi)
+        intersects .= false
+        ### ensure that each fire is only counted once per read
+        for (s, l, q) in firemsps(record, recorddata)
+            if q >= 0.9 * 255
+                genstart, genstop = genomecoords(s, l, record, recorddata)
+                if !iszero(genstart) && !iszero(genstop)
+
+                    @inbounds @simd for i = eachindex(fireintersections)
+                        if (genstart <= fireintersections[i].last) && (genstop >= fireintersections[i].first)
+                            # k = GenomicFeatures.metadata(fireintersections[i])
+                            # fire_counts[k] += 1
+                            intersects[i] = true
+                        end
+                    end
+                end
+            end
+        end
+
+        for mi in ModIterator(record, recorddata)
+            
+            genomepos = genomecoords(mi.pos, record, recorddata)
+            
+            @inbounds @simd for i = eachindex(fireintersections)
+                if (genomepos <= fireintersections[i].last) && (genomepos >= fireintersections[i].first)
+                    k = GenomicFeatures.metadata(fireintersections[i])
+                    if mi.prob >= 0.9 * 255
+                        fg_counts[Symbol(mi.mod)][k] += 1
+                    end
+                    total_counts[Symbol(mi.mod)][k] += 1
+                end
+            end
+        end
+
+        for i = eachindex(intersects)
+            if intersects[i]
+                k = GenomicFeatures.metadata(fireintersections[i])
+                fg_counts[:fire][k] += 1
+            end
+        end
+
+        t += 1
+        (t == total_reads) && break
+    end
+    close(reader)
+
+    fg_counts, total_counts
+
+end
